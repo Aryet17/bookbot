ee8735dbf82619f14b4b599a1599015b93ede909
Aryet17 <Balserayeray@gmail.com>
2025-02-06 21:53:40 +0100

    main.py updated

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    many_words = count_words(text)
+    print (f"{many_words} son las veces que te voy a besar")
+
+# Abre el archivo y guarda el contenido en una variable
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+            
+# divide el string y mide cuantas palabras son
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+main()    
